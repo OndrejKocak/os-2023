1c4ca3878f1c2d44cc6772b414b96f2c110f2f89
Ondrej Kocak <ondrej.kocak@gmail.com>
2023-09-25 17:39:00 +0200

    feat: primes

---
+++ user/primes.c
@@ -0,0 +1,43 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+void primeSieve(int p[]){
+    int forkRet = fork();
+        if(forkRet == 0){
+            int prime = 0;
+            int n;
+            if(read(p[0], &prime, sizeof(int)) == 0){
+                exit(0);
+            }
+            fprintf(1, "prime %d\n", prime);
+            int tmp[2];
+            pipe(tmp);
+            while(read(p[0], &n, sizeof(int))){
+                if(n%prime != 0){
+                    write(tmp[1], &n, sizeof(int));
+                }
+            }
+            close(p[0]);
+            close(tmp[1]);
+            primeSieve(tmp);
+        }
+        
+        
+        close(p[0]);
+        wait(0);
+}
+
+int main(int argc, char *argv[]){
+    int p[2];
+    pipe(p);
+    int arr[34];
+    for(int j = 2, i = 0; j <= 35; j++, i++){
+        arr[i] = j;
+        write(p[1], &arr[i], sizeof(int));
+    }
+    close(p[1]);
+    primeSieve(p);
+    exit(0);
+}
