9faa12760d0528ffad354aedb8ff17c4ecec7a34
Ondrej Kocak <ondrej.kocak@gmail.com>
2023-09-30 10:39:02 +0200

    feat: primes refactor

--- user/primes.c
+++ user/primes.c
@@ -4,35 +4,32 @@
 #include "user/user.h"
 
 void primeSieve(int p[]){
-    int forkRet = fork();
-        if(forkRet == 0){
-            int prime = 0;
-            int n;
-            if(read(p[0], &prime, sizeof(int)) == 0){
-                exit(0);
-            }
-            fprintf(1, "prime %d\n", prime);
-            int tmp[2];
-            pipe(tmp);
-            while(read(p[0], &n, sizeof(int))){
-                if(n%prime != 0){
-                    write(tmp[1], &n, sizeof(int));
-                }
-            }
-            close(p[0]);
-            close(tmp[1]);
-            primeSieve(tmp);
+    if (fork() == 0){
+        int prime = 0;
+        int n;
+        if (read(p[0], &prime, sizeof(int)) == 0){
+            exit(0);
         }
-        
-        
+        fprintf(1, "prime %d\n", prime);
+        int tmp[2];
+        pipe(tmp);
+        while (read(p[0], &n, sizeof(int))){
+            if (n % prime != 0){
+                write(tmp[1], &n, sizeof(int));
+            }
+        }
         close(p[0]);
-        wait(0);
+        close(tmp[1]);
+        primeSieve(tmp);
+    }
+    close(p[0]);
+    wait(0);
 }
 
 int main(int argc, char *argv[]){
     int p[2];
     pipe(p);
-    for(int j = 2; j <= 35; j++){
+    for (int j = 2; j <= 35; j++){
         write(p[1], &j, sizeof(int));
     }
     close(p[1]);
